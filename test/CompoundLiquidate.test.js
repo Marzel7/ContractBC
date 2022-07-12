f4ca32dcd98768c6d99266e88096a82b8976d3ea
kkeaveney <kevinkeaveney@gmail.com>
2022-07-12 16:24:49 +0100

    init Compound Borrow

--- test/CompoundLiquidate.test.js
+++ test/CompoundLiquidate.test.js
@@ -20,12 +20,13 @@ const C_TOKEN_BORROW = CDAI;
 const LIQUIDATOR = DAI_WHALE;
 
 const SUPPLY_DECIMALS = 8;
-const SUPPLY_AMOUNT = String(pow(10, SUPPLY_DECIMALS).mul(new BN(10)));
+const SUPPLY_AMOUNT = String(pow(10, SUPPLY_DECIMALS).mul(new BN(1)));
 const BORROW_DECIMALS = 18;
 
 let testCompound;
 let tokenSupply;
 let tokenBorrow;
+let cTokenSupply;
 let liquidator;
 let signer;
 let deployer;
@@ -58,7 +59,7 @@ beforeEach(async () => {
   cTokenSupply = await ethers.getContractAt(CERC20, C_TOKEN_SUPPLY, signer);
   tokenBorrow = await ethers.getContractAt(IERC20, TOKEN_BORROW, signer);
 
-  const supplyBal = await tokenSupply.balanceOf(SUPPLY_WHALE);
+  let supplyBal = await tokenSupply.balanceOf(SUPPLY_WHALE);
   console.log(`supply whale balance: ${supplyBal.div(String(pow(10, SUPPLY_DECIMALS)))}`);
   assert(supplyBal.gte(SUPPLY_AMOUNT), "bal < supply");
 
@@ -96,15 +97,16 @@ describe("liquidation", async () => {
     let tx;
     let snap;
     snap = await snapshot(testCompound, liquidator);
-    console.log(`liquidity: ${snap.liquidity}`);
-    console.log(`supplied: ${snap.supplied}`);
+    console.log(`liquidity: $${snap.liquidity}`);
+    console.log(`supplied: ${snap.supplied} wbtc`);
+    console.log(`colFactor: ${snap.colFactor} %`);
     // supply
     await tokenSupply.connect(signer).approve(testCompound.address, SUPPLY_AMOUNT);
     (tx = await testCompound.connect(signer).supply(SUPPLY_AMOUNT)), {gasLimit: 500000};
     snap = await snapshot(testCompound, liquidator);
     console.log(`--- supplied ---`);
     console.log(`col factor: ${snap.colFactor} %`);
-    console.log(`supplied: ${snap.supplied}`);
+    console.log(`supplied: ${snap.supplied} wbtc`);
 
     // enter market
     tx = await testCompound.connect(user1).enterMarket();
@@ -113,42 +115,42 @@ describe("liquidation", async () => {
     const price = await testCompound.getPriceFeed(C_TOKEN_BORROW);
     const maxBorrow = liquidity.mul(String(pow(10, BORROW_DECIMALS))).div(price);
     // tweak borrow amount if borrow fails
-    const borrowAmount = maxBorrow.mul(9998).div(10000);
+    const borrowAmount = maxBorrow.mul(9996).div(10000);
 
     console.log("----- entered market -----");
-    console.log(`liquidity: ${liquidity.div(String(pow(10, 18)))}`);
-    console.log(`price: ${price.div(String(pow(10, 18)))}`);
-    console.log(`max borrow ${maxBorrow.div(String(pow(10, 18)))}`);
-    console.log(`borrow amount: ${borrowAmount.div(String(pow(10, 18)))}`);
+    console.log(`liquidity: $${liquidity.div(String(pow(10, 18)))}`);
+    console.log(`price: $${price.div(String(pow(10, 18)))}`);
+    console.log(`max borrow $${maxBorrow.div(String(pow(10, 18)))}`);
+    console.log(`borrow amount: $${borrowAmount.div(String(pow(10, 18)))}`);
 
     (tx = await testCompound.borrow(borrowAmount)), {gasLimit: 500000};
 
     snap = await snapshot(testCompound, liquidator);
     console.log(`---- borrowed ----`);
-    console.log(`liquidity: ${snap.liquidity}`);
-    console.log(`borrowed: ${snap.borrowed}`);
+    console.log(`liquidity: $${snap.liquidity}`);
+    console.log(`borrowed: $${snap.borrowed}`);
 
     // accrue interest on borrow
     const block = await web3.eth.getBlockNumber();
     // NOTE: tweak this to increase borrowed amount
-    await time.advanceBlockTo(block + 90000);
+    await time.advanceBlockTo(block + 120000);
     // send any tx to Compound to update liquidity and shortfall
     await testCompound.getBorrowBalance();
 
     snap = await snapshot(testCompound, liquidator);
     console.log(`--- after some blocks ---`);
-    console.log(`liquidity: $ ${snap.liquidity}`);
-    console.log(`shortfall: $ ${snap.shortfall}`);
-    console.log(`borrowed: ${snap.borrowed}`);
+    console.log(`liquidity: $${snap.liquidity}`);
+    console.log(`shortfall: $${snap.shortfall}`);
+    console.log(`borrowed: $${snap.borrowed}`);
 
     // liquidate
     const closeFactor = await liquidator.getCloseFactor();
     const repaymentAmount = await testCompound.callStatic.getBorrowBalance();
     repayAmount = repaymentAmount.mul(closeFactor).div(String(pow(10, 18)));
 
-    const liqBal = await tokenBorrow.balanceOf(LIQUIDATOR);
+    let liqBal = await tokenBorrow.balanceOf(LIQUIDATOR);
     console.log("---- liquidation ----");
-    console.log(`liquidator balance: ${liqBal.div(String(pow(10, BORROW_DECIMALS)))}`);
+    console.log(`liquidator DAI balance: ${liqBal.div(String(pow(10, BORROW_DECIMALS)))}`);
     assert(liqBal.gte(repayAmount, "bal < repay"));
 
     const amountToBeLiquidated = await liquidator.getAmountToBeLiquidated(C_TOKEN_BORROW, C_TOKEN_SUPPLY, repayAmount);
@@ -157,11 +159,24 @@ describe("liquidation", async () => {
         amountToBeLiquidated.div(String(pow(10, SUPPLY_DECIMALS - 2))) / 100
       }`
     );
+
+    // supplyBal = await tokenSupply.balanceOf(SUPPLY_WHALE);
+    // console.log(`supply whale balance: ${supplyBal.div(String(pow(10, SUPPLY_DECIMALS)))}`);
+
+    // let testCompound_WBTC_bal = await tokenSupply.balanceOf(testCompound.address);
+    // console.log(`TestCompound WBTC balanace: ${testCompound_WBTC_bal.div(String(pow(10, SUPPLY_DECIMALS)))}`);
+
+    let liquidator_cWBTC_balance = await cTokenSupply.balanceOf(LIQUIDATOR);
+    console.log(`Liquidator cWBTC balance: ${liquidator_cWBTC_balance.div(String(pow(10, SUPPLY_DECIMALS)))}`);
+
+    let testCompound_cWBTC_balance = await cTokenSupply.balanceOf(testCompound.address);
+    console.log(`TestCompound cWBTC balance: ${testCompound_cWBTC_balance.div(String(pow(10, SUPPLY_DECIMALS)))}`);
+
     await tokenBorrow.connect(signer_LIQUIDATOR).approve(liquidator.address, repayAmount);
     tx = await liquidator.connect(signer_LIQUIDATOR).liquidate(testCompound.address, repayAmount, C_TOKEN_SUPPLY);
 
     snap = await snapshot(testCompound, liquidator);
-    console.log(`--- liquidated ---`);
+    console.log(`--- liquidate ---`);
     console.log(`close factor: ${snap.closeFactor} %`);
     console.log(`liquidation incentive: ${snap.incentive}`);
     console.log(`supplied: ${snap.supplied}`);
@@ -169,5 +184,18 @@ describe("liquidation", async () => {
     console.log(`shortfall: $ ${snap.shortfall}`);
     console.log(`borrowed: ${snap.borrowed}`);
     console.log(`liquidated: ${snap.liquidated}`);
+
+    console.log(`---- balances ----`);
+    liqBal = await tokenBorrow.balanceOf(LIQUIDATOR);
+    console.log(`liquidator DAI balance: ${liqBal.div(String(pow(10, BORROW_DECIMALS)))}`);
+
+    liquidator_cWBTC_bal = await cTokenSupply.balanceOf(liquidator.address);
+    console.log(`liquidator cWBTC balance: ${liquidator_cWBTC_bal.div(String(pow(10, SUPPLY_DECIMALS)))}`);
+
+    console.log(`Liquidator WBTC balance: ${snap.liquidated}`);
+    console.log(`TestCompound WBTC balance: ${snap.supplied}`);
+
+    testCompound_cDAI_balance = await cTokenSupply.balanceOf(testCompound.address);
+    console.log(`TestCompound cWBTC balance: ${testCompound_cDAI_balance.div(String(pow(10, SUPPLY_DECIMALS)))}`);
   });
 });
